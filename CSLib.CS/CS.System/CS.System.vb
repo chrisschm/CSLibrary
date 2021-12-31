bdbea6b748c58d98cf0b687d49a70a2c34c92760
Christian Schmidt <47923480+chrisschm@users.noreply.github.com>
2021-12-31 01:15:06 +0100

    Add files via upload

---
+++ CSLib.CS/CS.System/CS.System.vb
@@ -0,0 +1,251 @@
+﻿Namespace Global
+    Namespace CS
+        Namespace System
+
+            Public Class Config
+
+                Public Event RebootNedded()
+
+                Public Property RealTimeIsUniversal() As Boolean
+                    Get
+                        Dim readValue As Integer = Registry.GetValue("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation", "RealTimeIsUniversal", 0)
+                        Return readValue
+                    End Get
+                    Set(value As Boolean)
+                        Registry.SetValue("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation", "RealTimeIsUniversal", value)
+                    End Set
+                End Property
+
+
+            End Class
+
+
+
+            Public Class Services
+
+                Implements IEnumerable
+
+                Private colServices As Collection
+
+#Region "CreateObject"
+                ''' <summary>
+                ''' Erstellt eine neue Instanz der Services Klasse und gibt ein Services Objekt zurück.
+                ''' </summary>
+                Public Sub New()
+
+                    Dim scs() As ServiceProcess.ServiceController
+                    Dim sc As Service
+
+                    scs = ServiceProcess.ServiceController.GetServices()
+
+                    For Each s As ServiceProcess.ServiceController In scs
+                        sc = New Service(s.ServiceName, s.MachineName)
+                        colServices.Add(sc, sc.ServiceName)
+                    Next
+
+                    scs = ServiceProcess.ServiceController.GetDevices()
+
+                    For Each s As ServiceProcess.ServiceController In scs
+                        sc = New Service(s.ServiceName, s.MachineName)
+                        colServices.Add(sc, sc.ServiceName)
+                    Next
+
+                End Sub
+
+                ''' <summary>
+                ''' Erstellt eine neue Instanz der Services Klasse und gibt ein Services Objekt zurück.
+                ''' </summary>
+                ''' <param name="Host">Erforderlich. Hostname des abzufragenden Computers. Wird keiner angegeben, werden die Dienste des lokalen Computers abgefragt.</param>
+                Public Sub New(Host As String)
+
+                    Dim scs() As ServiceProcess.ServiceController
+                    Dim sc As Service
+
+                    scs = ServiceProcess.ServiceController.GetServices(Host)
+
+                    For Each s As ServiceProcess.ServiceController In scs
+                        sc = New Service(s.ServiceName, s.MachineName)
+                        colServices.Add(sc, sc.ServiceName)
+                    Next
+
+                    scs = ServiceProcess.ServiceController.GetDevices(Host)
+
+                    For Each s As ServiceProcess.ServiceController In scs
+                        sc = New Service(s.ServiceName, s.MachineName)
+                        colServices.Add(sc, sc.ServiceName)
+                    Next
+
+                End Sub
+
+                ''' <summary>
+                ''' Gibt einen Boolean Wert zurück der angibt, ob ein Services-Objekt 
+                ''' ein Element mit einem bestimmten Schlüssel enthält.
+                ''' </summary>
+                ''' <param name="Key">
+                ''' Erforderlich. Ein String Ausdruck, der den Schlüssel für die
+                ''' Elemente der Auflistung übergibt, die gesucht werden sollen.
+                ''' </param>
+                ''' <returns>
+                ''' Gibt einen Boolean Wert zurück der angibt, ob ein Services-Objekt 
+                ''' ein Element mit einem bestimmten Schlüssel enthält.
+                ''' </returns>
+                Public Function Contains(Key As String) As Boolean
+                    Return colServices.Contains(Key)
+                End Function
+
+
+                Public Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+                    Return colServices.GetEnumerator()
+                End Function
+#End Region
+
+
+
+
+#Region "PublicProperties"
+
+                Public ReadOnly Property Count() As Integer
+                    Get
+                        Return colServices.Count()
+                    End Get
+                End Property
+
+                Public ReadOnly Property Item(Index As Integer) As Service
+                    Get
+                        Return colServices.Item(Index)
+                    End Get
+                End Property
+
+                Public ReadOnly Property Item(Index As Object) As Service
+                    Get
+                        Return colServices.Item(Index)
+                    End Get
+                End Property
+
+                Public ReadOnly Property Item(Key As String) As Service
+                    Get
+                        Return colServices.Item(Key)
+                    End Get
+                End Property
+#End Region
+
+            End Class
+
+
+
+
+
+
+            Public Class Service
+
+                Private SC As ServiceProcess.ServiceController
+
+                Friend Sub New(Name As String)
+
+                    SC = New ServiceProcess.ServiceController(Name)
+
+                End Sub
+
+                Friend Sub New(Name As String, Host As String)
+
+                    SC = New ServiceProcess.ServiceController(Name, Host)
+
+                End Sub
+
+
+
+
+                Public ReadOnly Property CanPauseAndContinue As Boolean
+                    Get
+                        Return SC.CanPauseAndContinue
+                    End Get
+                End Property
+
+                Public ReadOnly Property CanShutdown As Boolean
+                    Get
+                        Return CanShutdown
+                    End Get
+                End Property
+
+                Public ReadOnly Property CanStop As Boolean
+                    Get
+                        Return SC.CanStop
+                    End Get
+                End Property
+
+                Public ReadOnly Property DependentServices As Service()
+                    Get
+                        Dim tempSCs(SC.DependentServices.Count) As Service
+                        Dim tempSC As Service
+                        Dim l As Long = 0
+
+                        For Each s As ServiceProcess.ServiceController In SC.DependentServices
+                            tempSC = New Service(s.ServiceName, s.MachineName)
+                            tempSCs(l) = tempSC
+                            l = l + 1
+                        Next
+
+                        Return tempSCs
+                    End Get
+                End Property
+
+                Public Property DisplayName As String
+                    Get
+                        Return SC.DisplayName
+                    End Get
+                    Set(value As String)
+                        SC.DisplayName = value
+                    End Set
+                End Property
+
+                Public ReadOnly Property MachineName As String
+                    Get
+                        Return SC.MachineName
+                    End Get
+                End Property
+
+                Public ReadOnly Property ServiceName As String
+                    Get
+                        Return SC.ServiceName
+                    End Get
+                End Property
+
+                Public ReadOnly Property ServicesDependedOn As Service()
+                    Get
+                        Dim tempSCs(SC.ServicesDependedOn.Count) As Service
+                        Dim tempSC As Service
+                        Dim l As Long = 0
+
+                        For Each s As ServiceProcess.ServiceController In SC.ServicesDependedOn
+                            tempSC = New Service(s.ServiceName, s.MachineName)
+                            tempSCs(l) = tempSC
+                            l = l + 1
+                        Next
+
+                        Return tempSCs
+                    End Get
+                End Property
+
+                Public ReadOnly Property StartType As ServiceProcess.ServiceStartMode
+                    Get
+                        Return SC.StartType
+                    End Get
+                End Property
+
+                Public ReadOnly Property Status As ServiceProcess.ServiceControllerStatus
+                    Get
+                        Return SC.Status
+                    End Get
+                End Property
+
+                Public ReadOnly Property Type As ServiceProcess.ServiceType
+                    Get
+                        Return SC.ServiceType
+                    End Get
+                End Property
+
+            End Class
+
+        End Namespace
+    End Namespace
+End Namespace
---
+++ CSLib.CS/CS.System/Management.vb
@@ -0,0 +1,89 @@
+﻿Namespace CS
+    Namespace System
+
+        Public Module Management
+
+            ''' <summary>
+            ''' The shutdown type. This parameter must include one of the following values. Optionally include one of the Force flags.
+            ''' </summary>
+            Public Enum ExitWindowsFlags As Integer
+                HybridShutdown = &H400000
+                Logoff = &H0
+                Poweroff = &H8
+                Reboot = &H2
+                RestartApps = &H40
+                Shutdown = &H1
+                Force = &H4
+                ForceIfHung = &H10
+            End Enum
+
+            ''' <summary>
+            ''' Major reason flags indicate the general issue type. Minor reason flags modify the specified 
+            ''' major reason flag. You can use any minor reason in conjunction with any major reason, but 
+            ''' some combinations do not make sense. Planned and UserDefined flags provide additional information about the event.
+            ''' </summary>
+            Public Enum SystemShutdownReasonCodes As Integer
+                MajorApplication = &H40000
+                MajorHardware = &H10000
+                MajorLegacyAPI = &H70000
+                MajorOS = &H20000
+                MajorOther = &H0
+                MajorPower = &H60000
+                MajorSoftware = &H30000
+                MajorSystem = &H50000
+                MinorBluescreen = &HF
+                MinorCordunplugge = &HB
+                MinorDisk = &H7
+                MinorEnvironment = &HC
+                MinorHardwareDriver = &HD
+                MinorHotfix = &H11
+                MinorHotfixUninstall = &H17
+                MinorHung = &H5
+                MinorInstallation = &H2
+                MinorMaintenance = &H1
+                MinorMMC = &H19
+                MinorNetworkConnectivity = &H14
+                MinorNetworkCard = &H9
+                MinorOther = &H0
+                MinorOtherDriver = &HE
+                MinorPowerSupply = &HA
+                MinorProcessor = &H8
+                MinorReconfig = &H4
+                MinorSecurity = &H13
+                MinorSecurityFix = &H12
+                MinorSecurityFixUninstall = &H18
+                MinorServicePack = &H10
+                MinorServicePackUninstall = &H16
+                MinorTermSRV = &H20
+                MinorUnstable = &H6
+                MinorUpgrade = &H3
+                MinorWMI = &H15
+                UserDefined = &H40000000
+                Planned = &H80000000
+            End Enum
+
+            Private Declare Function ExitWindowsEx Lib "user32.dll" (ByVal uFlags As Int32, ByVal dwReserved As Int32) As Int32
+
+            ''' <summary>
+            ''' Logs off the interactive user, shuts down the system, or shuts down and restarts the system. It sends the 
+            ''' WM_QUERYENDSESSION message to all applications to determine if they can be terminated.
+            ''' </summary>
+            ''' <param name="Flags">
+            ''' The shutdown type. This parameter must include one of the following values.
+            ''' </param>
+            ''' <param name="Reason">
+            ''' The reason for initiating the shutdown. This parameter must be one of the system shutdown reason codes.
+            ''' </param>
+            ''' <returns>
+            ''' If the function succeeds, the return value is nonzero. Because the function executes asynchronously, 
+            ''' a nonzero return value indicates that the shutdown has been initiated. It does not indicate whether 
+            ''' the shutdown will succeed. It is possible that the system, the user, or another application will abort the shutdown.
+            ''' </returns>
+            Public Function ExitWindows(ByVal Flags As ExitWindowsFlags, ByVal Optional Reason As SystemShutdownReasonCodes = SystemShutdownReasonCodes.MajorOther) As Integer
+                Return ExitWindowsEx(Flags, Reason)
+            End Function
+
+        End Module
+
+    End Namespace
+End Namespace
